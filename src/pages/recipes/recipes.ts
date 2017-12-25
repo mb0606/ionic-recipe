a1dfd7b74d841ea376e09fcd76a1518001110bf0
Marco Berardini <marcoberardini@gmail.com>
2017-12-24 16:29:33 -0800

    Display added ingredients on shopping list page. Added plus button to recipe page.

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EditRecipePage } from '../edit-recipe/edit-recipe';
 
 /**
  * Generated class for the RecipesPage page.
@@ -18,8 +19,8 @@ export class RecipesPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RecipesPage');
+  onNewRecipe(){
+    this.navCtrl.push(EditRecipePage);
   }
 
 }
--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../../models/ingredient';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -9,17 +10,31 @@ import { ShoppingListService } from './../../services/shopping-list.service';
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
-  list:any;
+  list: Ingredient[];
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private _shoppingListService: ShoppingListService) {
   }
 
+  ionViewWillEnter(){
+    this.loadItems();
+    }
+
   onAddItem(form){
     console.log(form.value);
     this._shoppingListService.addItem(form.value.ingredientName, form.value.amount);
-    this.list = this._shoppingListService.getItems()
+    form.reset();
+    this.loadItems();
     console.log("list from service" ,this.list);
+  }
+
+  private loadItems(){
+    this.list = this._shoppingListService.getItems();     
+  }
+
+  deleteItem(index: number){
+    this._shoppingListService.removeItem(index);
+    this.loadItems();    
   }
 
 }
