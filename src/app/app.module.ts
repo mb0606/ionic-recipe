4c09f7f3058cf312e97d4e7dae5054dc5a62337f
Marco Berardini <marcoberardini@gmail.com>
2017-12-25 13:37:15 -0800

    Create Recipe class and Recipes service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { ShoppingListService } from './../services/shopping-list.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+// Pages
 import { MyApp } from './app.component';
 import { TabsPage } from './../pages/tabs/tabs';
 import { ShoppingListPage } from './../pages/shopping-list/shopping-list';
 import { RecipesPage } from './../pages/recipes/recipes';
 import { RecipePage } from './../pages/recipe/recipe';
 import { EditRecipePage } from './../pages/edit-recipe/edit-recipe';
+// Services
+import { RecipeService } from '../services/recipes.service';
+import { ShoppingListService } from './../services/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -38,6 +40,7 @@ import { EditRecipePage } from './../pages/edit-recipe/edit-recipe';
     StatusBar,
     SplashScreen,
     ShoppingListService,
+    RecipeService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
